e1dad75d672e8ee479a8d32745ecdd6678c8c79e
seabaaple <tonypham700@gmail.com>
2023-06-22 23:59:01 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    // Green -> arrows on left for start of code to run
+// Class just like in C#
+    public static void main(String[] args) {
+// Press Alt+Enter with your caret at the highlighted text to see how
+// IntelliJ IDEA suggests fixing it.
+//Commenting out code is the same as C#
+// Methods in Java are lowercase instead of uppercase in C#
+        printExample1(); //Runs the method1 I created below the Main class
+
+
+
+
+
+
+
+    } // main
+
+
+
+// Classwork practice Java Functions from teacher: https://github.com/WCramRTC/Prog_225_S23_L10_Intro_To_Java_Part2/blob/main/src/com/wcram/Main.java
+
+    public static void printExample1() {
+        System.out.println("Hello World! (testing println)"); //println goes to the next line
+        System.out.printf("testing printf (DOESN'T GO TO NEXT LINE!) ");
+        System.out.println("");
+        System.out.print("testing the minimal wording 'print' ");
+    }
+
+
+
+}
